0ac409cec029e8c50fc6408f35105e1f8dd2a23c
Andrey Dubrovskiy <dubrovskiy0@gmail.com>
2017-03-13 23:24:59 +0200

    Тесты

--- javaTasks/3_ReadingFile/src/ua/dubrovskiy/tests/ReadingFileTest.java
+++ javaTasks/3_ReadingFile/src/ua/dubrovskiy/tests/ReadingFileTest.java
@@ -2,23 +2,25 @@ package ua.dubrovskiy.tests;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.*;
 import static ua.dubrovskiy.java.ReadingFile.findTop100K;
 
 /**
  * Created by Andrey on 13.03.2017.
  */
-class ReadingFileTest extends TestCase {
-    @org.junit.Test
-    public void testfindTop100K() throws Exception {
-        List<String> top100 = findTop100K("1.txt", " ");
+public class ReadingFileTest extends TestCase {
+    @Test
+    public void testFindTop100K() throws Exception {
+        List<String> top = findTop100K("1.txt", " ");
         int i = 5;
-        for(String s : top100) {
+        for (String s : top) {
             String template = "Фраза " + i;
             if (!s.equals(template)) Assert.fail();
         }
     }
 }
+
